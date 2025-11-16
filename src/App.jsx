3b911d43440face8606ca4a3bffa09a2487349c8
selmirmne-tech <selmirmne@gmail.com>
2025-11-16 01:28:55 +0100

    Update slika

--- src/App.jsx
+++ src/App.jsx
@@ -879,7 +879,7 @@ if (authLoading) {
         <div className="p-3">
           <div className="d-flex justify-content-between align-items-center mb-3">
      <div className="d-flex align-items-center">
-  <img src={logo} alt="Logo" width="300" className="me-2" />
+  <img src={logo} alt="Logo" width="200" className="me-2" />
   <h4
     style={{
       fontWeight: "bold",
